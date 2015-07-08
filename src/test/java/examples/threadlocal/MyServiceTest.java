3ca075965b9af1bbb5e4189a4cbcac2d59736418
Tom Wigington <twigington@rgare.com>
2015-07-08 15:23:35 -0500

    A little more documentation and cleanup

--- src/test/java/examples/threadlocal/MyServiceTest.java
+++ src/test/java/examples/threadlocal/MyServiceTest.java
@@ -9,8 +9,16 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 /**
+ * Various examples of dealing with a ThreadLocal Subclass.
+ * <b/>
+ * I would recommend against creating a class like {@link MyThreadLocal}. Favor localized
+ * ThreadLocal variables as opposed to this "thread global" approach.
  *
+ * @see <a href="https://docs.oracle.com/javase/8/docs/api/java/lang/ThreadLocal.html">https://docs.oracle.com/javase/8/docs/api/java/lang/ThreadLocal.html</a>
+ * @see <a href="https://plumbr.eu/blog/java/when-and-how-to-use-a-threadlocal">https://plumbr.eu/blog/java/when-and-how-to-use-a-threadlocal</a>
+ * @see <a href="http://tutorials.jenkov.com/java-concurrency/threadlocal.html">http://tutorials.jenkov.com/java-concurrency/threadlocal.html</a>
  */
+@SuppressWarnings("UnusedParameters")
 public class MyServiceTest {
     @Test
     public void noMocking() {
@@ -19,92 +27,9 @@ public class MyServiceTest {
         assertThat(myService.getMyThreadLocalModule(), is("Default Module"));
     }
 
-    @Test
-    public void partialMockThreadLocalGetModule(@Mocked("getModule") final MyThreadLocal unused) {
-        final String expectedModule = "Mock Module";
-        //noinspection UnusedDeclaration
-        new Expectations() {{
-            MyThreadLocal.getThreadLocalCtx().getModule();
-            result = expectedModule;
-        }};
-
-        MyService myService = new MyService();
-
-        assertThat(myService.getMyThreadLocalModule(), is(expectedModule));
-    }
-
-    @Ignore("jmockit ends up in a loop and a StackOverflow error results")
-    @Test
-    public void dynamicallyMockThreadLocalGetModule_instanceMocking() {
-        final MyThreadLocal myThreadLocal = MyThreadLocal.getThreadLocalCtx();
-
-        final String expectedModule = "Mock Module";
-        //noinspection UnusedDeclaration
-        new Expectations(myThreadLocal) {{
-            myThreadLocal.getModule();
-            result = expectedModule;
-        }};
-
-        MyService myService = new MyService();
-
-        assertThat(myService.getMyThreadLocalModule(), is(expectedModule));
-    }
-
-    @Ignore("jmockit ends up in a loop and a StackOverflow error results")
-    @Test
-    public void dynamicallyMockThreadLocalGetModule_classMocking() {
-        final String expectedModule = "Mock Module";
-        //noinspection UnusedDeclaration
-        new Expectations(MyThreadLocal.class) {{
-            MyThreadLocal.getThreadLocalCtx().getModule();
-            result = expectedModule;
-        }};
-
-        MyService myService = new MyService();
-
-        assertThat(myService.getMyThreadLocalModule(), is(expectedModule));
-    }
-
-    @Ignore("jmockit ends up in a loop and a StackOverflow error results")
-    @Test
-    public void dynamicallyMockThreadLocalGetModule_classMockingWithLocalVal() {
-        final MyThreadLocal myThreadLocal = MyThreadLocal.getThreadLocalCtx();
-
-        final String expectedModule = "Mock Module";
-        //noinspection UnusedDeclaration
-        new Expectations(MyThreadLocal.class) {{
-            myThreadLocal.getModule();
-            result = expectedModule;
-        }};
-
-        MyService myService = new MyService();
-
-        assertThat(myService.getMyThreadLocalModule(), is(expectedModule));
-    }
-
-    @Test
-    public void partialMockedThreadLocal_mockedOtherService(
-            @Mocked("getOtherService") final MyThreadLocal unused,
-            @Mocked final OtherService otherService)
-    {
-        final String expectedOtherServiceName = "Mock Other Service";
-
-        //noinspection UnusedDeclaration
-        new Expectations() {{
-            MyThreadLocal.getThreadLocalCtx().getOtherService();
-            result = otherService;
-
-            otherService.getName();
-            result = expectedOtherServiceName;
-        }};
-
-        MyService myService = new MyService();
-
-        assertThat(myService.getOtherServiceName(), is(expectedOtherServiceName));
-    }
-
     /**
-     * This is the most sane pattern. But you need to be careful that you don't corrupt the thread for other tests.
+     * This seems to be the most sane pattern.
+     * You will need to be careful that you don't corrupt the thread local for other tests.
      */
     @Test
     public void noThreadLocalMock_mockedOtherService(@Mocked final OtherService otherService) {
@@ -120,5 +45,92 @@ public class MyServiceTest {
         MyService myService = new MyService();
 
         assertThat(myService.getOtherServiceName(), is(expectedOtherServiceName));
+    }
+
+    /**
+     * Will not work with version 1.19.
+     */
+    @Test
+    public void partialMockedThreadLocal_mockedOtherService(
+            @Mocked("getOtherService") final MyThreadLocal unused,
+            @Mocked final OtherService otherService)
+    {
+        final String expectedOtherServiceName = "Mock Other Service";
+
+        new Expectations() {{
+            MyThreadLocal.getThreadLocalCtx().getOtherService();
+            result = otherService;
+
+            otherService.getName();
+            result = expectedOtherServiceName;
+        }};
+
+        MyService myService = new MyService();
+
+        assertThat(myService.getOtherServiceName(), is(expectedOtherServiceName));
+    }
+
+    /**
+     * Will not work with version 1.19.
+     */
+    @Test
+    public void dynamicPartialThreadLocalMockGetModule_(@Mocked("getModule") final MyThreadLocal unused) {
+        final String expectedModule = "Mock Module";
+        new Expectations() {{
+            MyThreadLocal.getThreadLocalCtx().getModule();
+            result = expectedModule;
+        }};
+
+        MyService myService = new MyService();
+
+        assertThat(myService.getMyThreadLocalModule(), is(expectedModule));
+    }
+
+    @Ignore("jmockit ends up in a loop and a StackOverflow error results")
+    @Test
+    public void dynamicPartialThreadLocalMockGetModule_instanceMocking() {
+        final MyThreadLocal myThreadLocal = MyThreadLocal.getThreadLocalCtx();
+
+        final String expectedModule = "Mock Module";
+        new Expectations(myThreadLocal) {{
+            myThreadLocal.getModule();
+            result = expectedModule;
+        }};
+
+        MyService myService = new MyService();
+
+        assertThat(myService.getMyThreadLocalModule(), is(expectedModule));
+    }
+
+    @Ignore("jmockit ends up in a loop and a StackOverflow error results")
+    @Test
+    public void dynamicPartialThreadLocalMockGetModule_classMocking() {
+        final String expectedModule = "Mock Module";
+        //noinspection UnusedDeclaration
+        new Expectations(MyThreadLocal.class) {{
+            MyThreadLocal.getThreadLocalCtx().getModule();
+            result = expectedModule;
+        }};
+
+        MyService myService = new MyService();
+
+        assertThat(myService.getMyThreadLocalModule(), is(expectedModule));
+    }
+
+    @Ignore("jmockit ends up in a loop and a StackOverflow error results")
+    @Test
+    public void dynamicPartialThreadLocalMockGetModule_classMockingWithLocalVal() {
+        final MyThreadLocal myThreadLocal = MyThreadLocal.getThreadLocalCtx();
+
+        final String expectedModule = "Mock Module";
+        //noinspection UnusedDeclaration
+        new Expectations(MyThreadLocal.class) {{
+            myThreadLocal.getModule();
+            result = expectedModule;
+        }};
+
+        MyService myService = new MyService();
+
+        assertThat(myService.getMyThreadLocalModule(), is(expectedModule));
     }
 }
